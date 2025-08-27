d73464240405145bdb99a46922586a353bf24dc2
ABRAHAMAKHIL <akhilabraham34@gmail.com>
2025-08-27 19:59:44 +0530

    implemented api-gateway

---
+++ api-gateway/src/main/java/com/Mangrove/api_gateway/routes/GatewayRoutes.java
@@ -0,0 +1,41 @@
+package com.Mangrove.api_gateway.routes;
+
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.*;
+
+@Configuration
+public class GatewayRoutes {
+
+  @Bean
+  public RouterFunction<ServerResponse> productService() {
+
+    return GatewayRouterFunctions.route("product_service")
+        .route(
+            RequestPredicates.path("/product-service/**"),
+            HandlerFunctions.http("http://localhost:8060"))
+        .build();
+  }
+
+  @Bean
+  public RouterFunction<ServerResponse> inventoryService() {
+
+    return GatewayRouterFunctions.route("inventory_service")
+        .route(
+            RequestPredicates.path("/inventory-service/**"),
+            HandlerFunctions.http("http://localhost:8062"))
+        .build();
+  }
+
+  @Bean
+  public RouterFunction<ServerResponse> orderService() {
+
+    return GatewayRouterFunctions.route("order_service")
+        .route(
+            RequestPredicates.path("/order-service/**"),
+            HandlerFunctions.http("http://localhost:8061"))
+        .build();
+  }
+}
